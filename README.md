4482658f04fd6e94957b70512bbf1c29911d2df9
Jacob Stair <jacob.stair@gmail.com>
2020-02-01 08:38:30 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# aceholder
+# Aceholder Elements
+
 Placeholder elements for in-progress websites.
+
+## Usage
+
+    <div id="container">
+        <ace-holder paragraphs="3" words-per="55">
+            A label that will appear in the middle of the placeholder
+        </ace-holder>
+    </div>
+
+    <script>
+    (function () {
+        const aceholder = new Aceholder('#container');
+    }());
+    </script>
+
+## How it works
+
+The DOM is scanned for `<ace-holder>` elements and generates random text based on the option attributes.
+
+CSS then blurs the random text and adds some additional styles.
+
+The label (the innerHTML of the element) is then placed on top.
+
+All `<ace-holder>` elements in the given selector are processed. This allows different options to be set for different aceholders.
+
+If you are going to use the same settings for all aceholders, you can omit the selector and it will assume `'body'` as the selector.
+
+## Options
+
+An optional object of options can be set as a second argument.
+
+    const aceholder = new Aceholder('body', {});
+
+### Override generated classes
+
+The classes added to the generated child elements and the `<ace-holder>` element can be overridden with these options:
+
+    {
+        classes: {
+            isInit: 'aceholder-is-init', // Added to <ace-holder> after it is initialized
+            label: 'aceholder-label', // Added to the child <div> to mark it as the label
+            paragraph: 'aceholder-paragraph', // Added to each paragraph <div>
+            paragraphContainer: 'aceholder-paragraphs' // Added to the inner container <div> which holds the other elements
+        }
+    }
+
+### Adjust lengths of generated words
+
+The minimum and maximum length of generated words can be adjusted with these options:
+
+    {
+        lengths: {
+            wordMax: 10,
+            wordMin: 2
+        }
+    }
+
+## Todo
+
+I would like to add the following features in the future:
+
+ - Add the Javascript to properly register the `<ace-holder>` elements.
+ - Add support for image aceholders. More than likely this will be an optional `type="image"` attribute which will default to the standard text.
+ - Add some example images of the generated output to this readme.
---
+++ aceholder.js
@@ -0,0 +1,149 @@
+const ACEHOLDER_DEFAULT_OPTIONS = {
+    classes: {
+        isInit: 'aceholder-is-init',
+        label: 'aceholder-label',
+        paragraph: 'aceholder-paragraph',
+        paragraphContainer: 'aceholder-paragraphs'
+    },
+    lengths: {
+        wordMax: 10,
+        wordMin: 2
+    }
+};
+
+class Aceholder {
+    constructor(container, options) {
+        if (typeof container === 'undefined') {
+            container = 'body';
+        }// end if
+
+        if (typeof options !== 'object') {
+            options = {};
+        }// end if
+
+        this.container = (typeof container === 'object')
+            ? container
+            : document.querySelector(container);
+
+        this.options = Object.assign(ACEHOLDER_DEFAULT_OPTIONS, options);
+
+        if (this.container === null) {
+            throw 'An invalid container was specified.';
+        }// end if
+
+        this.populateElements();
+    }// end constructor()
+
+    /**
+	 * Generates a random integer between from and to.
+	 *
+	 * @param {number} from The lowest number to generate
+	 * @param {number} to The highest number to generate
+	 * @return {number} The generated number
+	 */
+    generateNumber(from, to) {
+        from = parseInt(from);
+        to = parseInt(to);
+
+        return (Math.floor(
+            from + (
+                (to - from + 1) * Math.random()
+            )
+        ));
+    }// end generateNumber()
+
+    /**
+	 * Generates a paragraph with {length} number of words.
+	 *
+	 * @param {number} length Number of words to generate
+	 * @return {string} The generated paragraph
+	 */
+    generateParagraph(length) {
+        let paragraph = [];
+
+        for (let i = 0; i < length; i += 1) {
+            paragraph.push(this.generateWord());
+        }// end for
+
+        return paragraph.join(' ');
+    }// end generateParagraph()
+
+    /**
+	 * Generates a random word.
+	 *
+	 * The size of the word is a random number of letters between the
+	 * wordMin and wordMax options.
+	 *
+	 * @return {string} The generated word
+	 */
+    generateWord() {
+        let max = this.options.lengths.wordMax,
+            min = this.options.lengths.wordMin,
+            length = this.generateNumber(min, max) - 1,
+            word = [];
+
+        word.push(this.generateNumber(65, 90));
+        for (let i = 0; i < length; i += 1) {
+            word.push(this.generateNumber(97, 122));
+        }// end for
+
+        return String.fromCharCode.apply(null, word);
+    }// end generateWord()
+
+    /**
+	 * Selects all uninitialized ace-holder elements and generates the
+	 * appropriate child elements.
+	 */
+    populateElements() {
+        let aceholders = Array.from(this.container.querySelectorAll(
+            'ace-holder:not(.' + this.options.classes.isInit + ')'
+        ));
+
+        aceholders.forEach((aceholder) => {
+            let labelContents = aceholder.innerHTML,
+                label = document.createElement('div'),
+                paragraphs = document.createElement('div'),
+                numParagraphs = (aceholder.hasAttribute('paragraphs'))
+                    ? parseInt(aceholder.getAttribute('paragraphs'))
+                    : 3,
+                wordsPer = (aceholder.hasAttribute('words-per'))
+                    ? parseInt(aceholder.getAttribute('words-per'))
+                    : 100;
+
+            aceholder.innerHTML = '';
+
+            paragraphs.classList.add(this.options.classes.paragraphContainer);
+            aceholder.append(paragraphs);
+
+            for (let i = 0; i < numParagraphs; i += 1) {
+                let paragraph = document.createElement('div');
+
+                paragraph.classList.add(this.options.classes.paragraph);
+                paragraph.innerHTML = this.generateParagraph(wordsPer);
+
+                paragraphs.append(paragraph);
+            }// end for
+
+            label.classList.add(this.options.classes.label);
+            label.innerHTML = labelContents;
+
+            paragraphs.append(label);
+
+            aceholder.classList.add(this.options.classes.isInit);
+        });
+    }// end populateElements()
+
+    /**
+	 * Selects any initialized ace-holder elements and returns them to
+	 * their original state.
+	 */
+    revertElements() {
+        let aceholders = Array.from(this.container.querySelectorAll(
+            'ace-holder.' + this.options.classes.isInit
+        ));
+
+        aceholders.forEach((aceholder) => {
+            //
+        });
+    }// end revertElements()
+}// end class Aceholder
